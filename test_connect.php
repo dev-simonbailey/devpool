e288f8d52ef1591eb8a5900c4fe95e6fac3b4bd1
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2020-12-12 19:03:26 +0000

    test connect update

--- test_connect.php
+++ test_connect.php
@@ -8,3 +8,9 @@ $DB = new MeekroDB($host, $user, $password, $dbName, $port, $encoding);
 $users = $DB->query("SELECT * FROM users");
 
 var_dump($users);
+
+foreach ($users as $user) {
+  foreach ($user as $key => $value) {
+    echo $key." ==> ".$value."\n";
+  }
+}
