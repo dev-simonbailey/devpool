4102a804c16d4b4322ce9e7200bd00a0cca40915
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2020-12-24 15:08:18 +0000

    update

--- test_connect.php
+++ test_connect.php
@@ -18,13 +18,29 @@ foreach ($skills as $value) {
 
   switch ($value['group']){
     case "apps_and_data":
-      echo $value['group']."==>".$value['skill']."<br />";
+      array_push($apps_and_data,$value['skill']);
+      //echo $value['group']."==>".$value['skill']."<br />";
       break;
     case "dev_ops":
-      echo $value['group']."==>".$value['skill']."<br />";
+      array_push($dev_ops,$value['skill']);
+      //echo $value['group']."==>".$value['skill']."<br />";
       break;
     case "utils":
-      echo $value['group']."==>".$value['skill']."<br />";
+      array_push($utils,$value['skill']);
+      //echo $value['group']."==>".$value['skill']."<br />";
       break;
   }
 }
+
+foreach ($apps_and_data as $value) {
+  echo "apps_and_data"."==>".$value."<br />";
+}
+echo "<br /><br />";
+foreach ($dev_ops as $value) {
+  echo "dev_ops"."==>".$value."<br />";
+}
+echo "<br /><br />";
+foreach ($utils as $value) {
+  echo "utils"."==>".$value."<br />";
+}
+echo "<br /><br />";
