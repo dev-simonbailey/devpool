8a358647bf78b427e41f64fd94066581e63e0ade
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2020-12-24 14:02:06 +0000

    update

--- test_connect.php
+++ test_connect.php
@@ -8,12 +8,15 @@ $DB = new MeekroDB($host, $user, $password, $dbName, $port, $encoding);
 $users = $DB->queryFirstRow("SELECT * FROM users WHERE username='simon.bailey'");
 
 $skills = $DB->query("SELECT * FROM devpool01.skillset WHERE user_id = '".$users['id']."'");
-
-var_dump($skills);
-
+ 
 foreach ($skills as $key => $value) {
   foreach ($value as $item_key => $item) {
-    echo $item_key."==>".$item."<br />";
+    switch ($item_key){
+      case "apps_and_data":
+        echo $item_key."==>".$item."<br />";
+        break;
+      default:
+        break;
+    }
   }
-  
 }
