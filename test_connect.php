e203627ca2fb1ed1159aaa739acc430bfcf3495e
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2020-12-24 14:33:54 +0000

    update

--- test_connect.php
+++ test_connect.php
@@ -10,12 +10,20 @@ $users = $DB->queryFirstRow("SELECT * FROM users WHERE username='simon.bailey'")
 $skills = $DB->query("SELECT * FROM devpool01.skillset WHERE user_id = '".$users['id']."'");
  
 
-
+$apps_and_data  = [];
+$dev_ops        = [];
+$utils          = [];
 
 foreach ($skills as $value) {
 
   switch ($value['group']){
     case "apps_and_data":
+      echo $value['group']."==>".$value['skill']."<br />";
+      break;
+    case "dev_ops":
+      echo $value['group']."==>".$value['skill']."<br />";
+      break;
+    case "utils":
       echo $value['group']."==>".$value['skill']."<br />";
       break;
   }
